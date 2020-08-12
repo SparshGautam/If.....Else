d6f0946ba54fab4fa86f176b84c1fc4d1e9adbe1
Sparsh Gautam <69500031+SparshGautam@users.noreply.github.com>
2020-08-11 22:24:35 -0700

    If...Else PT1.py

---
+++ If...Else PT1.py
@@ -0,0 +1,10 @@
+ant_weight = 1                    #variable ant_weight assigns the value to 1
+
+elephant_weight = 2000            #variable elephant_weight assigns the value to 2000
+
+if elephant_weight > ant_weight:  #the syntax of if is given here if 2000 is greater than 1 it will print elephant weight is more
+ print("elephant weight is more")  
+ 
+
+else:                             #the synatx of else statement
+ print("ant weight is more")      #here if 1 is greater than 2000 it will print ant weight is more
